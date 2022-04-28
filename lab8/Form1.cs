945df4f283d3b09e517bc8ea397d4cdaa4e6fe7f
Phan M. Hung <pmhungpro1@gmail.com>
2022-04-28 12:54:08 +0700

    Add files via upload

---
+++ lab8/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            double a = rnd.NextDouble();
+            if (a<0.5)
+            {
+                label1.Text = "Yes";
+            }
+            else
+            {
+                label1.Text = "No";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] answer_list = {"Yes", "No", "I don't know", "It is certain", "You may rely on it", "Absolutely no", "Depend on the weather today", "Ask again later" };
+            double p = 1.0/8.0;
+
+            Random rnd = new Random();
+            double A = rnd.NextDouble();
+            int ans_id = 0;
+            A -= p;
+
+            while (A>0)
+            {
+                A -= p;               
+                ans_id++;
+            };
+
+            label1.Text = answer_list[ans_id];
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
